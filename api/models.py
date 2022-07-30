addb802d4fb4d3a7951a76c270f29a5c91d7e25c
Ulysse Valdenaire <ulysse.valdenairepro@gmail.com>
2022-07-30 11:45:18 +0200

    create category and product models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    name = models.CharField(max_length=60)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.name
